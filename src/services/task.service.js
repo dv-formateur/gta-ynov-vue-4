686e333d75691072ba4c688dd57bcc07eeb77d54
Raphaël Charre <rcharre@oceaneconsulting.com>
2018-11-17 23:20:42 +0100

    Firefox date correction

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,10 +1,10 @@
 import Task from '../entities/task'
 export default class TaskService {
     static getAllTasksForUser(callback, max = Math.max(), user, project, date_start = 0, date_end = Math.max()){
-        var res = [new Task('task1-' + user.id + '-' + project.id, 'task1-' + user.id + '-' + project.id, 0, new Date('11-1-2018'), new Date('11-20-2018'), user.id, 1),
-            new Task('task2-' + user.id + '-' + project.id, 'task2-' + user.id + '-' + project.id, 1, new Date('11-4-2018'), new Date('11-5-2018'), user.id, 1),
-            new Task('task3-' + user.id + '-' + project.id, 'task3-' + user.id + '-' + project.id, 2, new Date('11-8-2018'), new Date('11-15-2018'), user.id, 1),
-            new Task('task4-' + user.id + '-' + project.id, 'task4-' + user.id + '-' + project.id, 3, new Date('11-2-2018'), new Date('11-10-2018'), user.id, 1)];
+        var res = [new Task('task1-' + user.id + '-' + project.id, 'task1-' + user.id + '-' + project.id, 0, new Date('2018-11-1'), new Date('2018-11-20'), user.id, 1),
+            new Task('task2-' + user.id + '-' + project.id, 'task2-' + user.id + '-' + project.id, 1, new Date('2018-11-4'), new Date('2018-11-5'), user.id, 1),
+            new Task('task3-' + user.id + '-' + project.id, 'task3-' + user.id + '-' + project.id, 2, new Date('2018-11-8'), new Date('2018-11-15'), user.id, 1),
+            new Task('task4-' + user.id + '-' + project.id, 'task4-' + user.id + '-' + project.id, 3, new Date('2018-11-2'), new Date('2018-11-10'), user.id, 1)];
         callback(user, res.filter(function(e){
             return project.id == e.project_id;
         }).sort((e1, e2)=>{e1.date_start < e2.date_start ? 1:-1}));
